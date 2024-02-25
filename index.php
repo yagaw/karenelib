353ce3da197dd8be4ba703f026e00670a26dc990
yagaw <leyagaw@gmail.com>
2024-02-25 14:07:11 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+#myInput {
+  background-image: url('/css/searchicon.png');
+  background-position: 10px 10px;
+  background-repeat: no-repeat;
+  width: 100%;
+  font-size: 16px;
+  padding: 12px 20px 12px 40px;
+  border: 1px solid #ddd;
+  margin-bottom: 12px;
+}
+
+#myTable {
+  border-collapse: collapse;
+  width: 100%;
+  border: 1px solid #ddd;
+  font-size: 18px;
+}
+
+#myTable th, #myTable td {
+  text-align: left;
+  padding: 12px;
+}
+
+#myTable tr {
+  border-bottom: 1px solid #ddd;
+}
+
+#myTable tr.header, #myTable tr:hover {
+  background-color: #f1f1f1;
+}
+
+.button {
+  background-color: #008CBA; /* Blue */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
+</head>
+<body>
+
+<h2>KECD ELibrary</h2>
+
+<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for Book.." title="Type in a name">
+
+<table id="myTable">
+  <tr class="header">
+    <th style="width:60%;">Title</th>
+    <th style="width:40%;">Read</th>
+  </tr>
+  
+ 
+  <?php
+$list = scandir("book");
+for($i=2; $i < count($list); $i++) {
+          $name = $list[$i];
+          // echo "<img src='photo/$name' width='200'>";
+          echo "
+          <tr>
+          <td  > $name </td>
+          <td><a href='book/$name' target='_blank' class='button'> Read </a></td>
+
+                  </tr>";
+
+        }
+?>
+</table>
+
+<script>
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
+</script>
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,10 @@
+/\*\*
+
+- Scans a directory and returns an array of PDF files.
+-
+- @param string $directory The directory to scan.
+- @return array An array of PDF file names.
+  \*/
+  function scanDirForPDFs($directory) {
+  // Implementation code here
+  }
